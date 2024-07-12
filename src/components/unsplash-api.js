43a2027f5659f07f1d32519af9b3fbd35cafce61
Eugene1380 <eugene.volobuyev@gmail.com>
2024-07-12 14:23:08 +0300

    Update

--- src/components/unsplash-api.js
+++ src/components/unsplash-api.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const ACCESS_KEY = "OCXpc-aD4tVdzFQ3rEUssXuHfC4jQ5xmid8L9XJMtzM";
+const ACCESS_KEY = "r1bv0cGSOPN9yma5sswCw-JahRtAD_RuqGm3j75US3w";
 axios.defaults.baseURL = "https://api.unsplash.com/";
-// https://unsplash.com/developers
-// axios.defaults.headers.common['Authorization'] = ACCESS_KEY;
 
-export default async function getImages(searchImage, page) {
+export default async function getImages(searchImage: string, page: number) {
   const response = await axios.get(`/search/photos`, {
     params: {
       client_id: ACCESS_KEY,
@@ -14,6 +12,5 @@ export default async function getImages(searchImage, page) {
       per_page: 12,
     },
   });
-  // console.log('response >> ', response);
   return response.data;
 }
